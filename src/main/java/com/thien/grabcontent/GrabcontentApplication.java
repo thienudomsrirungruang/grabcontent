cb84016cc990bcae27c6680ac188a478728805d2
Abhirat Termsirichit <abhirat.term@gmail.com>
2019-01-05 15:08:52 +0700

    First commit

---
+++ src/main/java/com/thien/grabcontent/GrabcontentApplication.java
@@ -0,0 +1,14 @@
+package com.thien.grabcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabcontentApplication.class, args);
+	}
+
+}
+
