acba1319551fb37cd7b0c899751cc93e7b9caa09
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2019-05-11 09:30:41 +0700

    Moved byViews to CartoonController

--- src/main/java/com/thien/grabcontent/controller/CartoonController.java
+++ src/main/java/com/thien/grabcontent/controller/CartoonController.java
@@ -1,7 +1,6 @@
 package com.thien.grabcontent.controller;
 
 import com.thien.grabcontent.dto.CartoonInfoDTO;
-import com.thien.grabcontent.entity.CartoonInfo;
 import com.thien.grabcontent.service.CartoonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,40 +15,45 @@ public class CartoonController {
     CartoonService cartoonService;
 
     @PostMapping
-    public void createCartoon(@RequestBody CartoonInfoDTO cartoonInfo){
+    public void createCartoon(@RequestBody CartoonInfoDTO cartoonInfo) {
         cartoonService.createCartoon(cartoonInfo);
     }
 
     @GetMapping("/{id}")
-    public CartoonInfoDTO getCartoonById(@PathVariable("id") Long id){
+    public CartoonInfoDTO getCartoonById(@PathVariable("id") Long id) {
         return cartoonService.getCartoonById(id);
     }
 
     @GetMapping
-    public List<CartoonInfoDTO> getAllCartoons(@RequestParam String page){
+    public List<CartoonInfoDTO> getAllCartoons(@RequestParam String page) {
         int pageInt = Integer.parseInt(page);
         return cartoonService.getAllCartoons(pageInt);
     }
 
     @PutMapping
-    public void updateCartoon(@RequestBody CartoonInfoDTO cartoonInfo){
+    public void updateCartoon(@RequestBody CartoonInfoDTO cartoonInfo) {
         cartoonService.updateCartoon(cartoonInfo);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCartoon(@PathVariable("id") Long id){
+    public void deleteCartoon(@PathVariable("id") Long id) {
         cartoonService.deleteCartoon(id);
     }
 
     @GetMapping("/cartoonNames")
-    public List<String> getCartoonNames(){
+    public List<String> getCartoonNames() {
         return cartoonService.getCartoonNames();
     }
 
     @GetMapping("/byName/{cartoon}")
-    public List<CartoonInfoDTO> getCartoonsByName(@PathVariable("cartoon") String cartoonName){
+    public List<CartoonInfoDTO> getCartoonsByName(@PathVariable("cartoon") String cartoonName) {
         return cartoonService.getCartoonsByName(cartoonName);
     }
 
-
+    @GetMapping("/api/cartoon/byViews")
+    @ResponseBody
+    public List<CartoonInfoDTO> getCartoonsByViews(@RequestParam String page) {
+        int pageInt = Integer.parseInt(page);
+        return cartoonService.getCartoonsByViews(pageInt);
+    }
 }
--- src/main/java/com/thien/grabcontent/controller/IndexController.java
+++ src/main/java/com/thien/grabcontent/controller/IndexController.java
@@ -1,15 +1,10 @@
 package com.thien.grabcontent.controller;
 
 
-import com.thien.grabcontent.dto.CartoonInfoDTO;
 import com.thien.grabcontent.service.CartoonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -22,10 +17,4 @@ public class IndexController {
         return "main";
     }
 
-    @GetMapping("/api/cartoon/byViews")
-    @ResponseBody
-    public List<CartoonInfoDTO> getCartoonsByViews(@RequestParam String page) {
-        int pageInt = Integer.parseInt(page);
-        return cartoonService.getCartoonsByViews(pageInt);
-    }
 }
