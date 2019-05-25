5d176cfdd43874b4068306292f6abbba3265e084
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2019-05-25 14:48:48 +0700

    Refractored code for GrabService

--- src/main/java/com/thien/grabcontent/service/GrabService.java
+++ src/main/java/com/thien/grabcontent/service/GrabService.java
@@ -10,7 +10,6 @@ import com.thien.grabcontent.repository.PageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -33,52 +32,55 @@ public class GrabService {
     @Autowired
     private WebService webService;
 
+
     public void getLatestCartoon(String cartoonName) throws Exception {
-        System.out.println("test");
-        List<CartoonConfig> cartoonList = cartoonProperties.getCartoonList();
+        String url = getUrlFromCartoon(cartoonName);
 
-        // check if cartoon is in cartoonList
-        boolean found = false;
-        String url = null;
-        for(CartoonConfig cartoon : cartoonList){
-            if(cartoon.getName().equals(cartoonName)){
-                found = true;
-                url = cartoon.getUrl();
-            }
-        }
-        if(!found){
-            throw new IllegalArgumentException("Cartoon not found");
-        }
+        int latestEpisodeStored = getLatestEpisodeNumberStored(cartoonName);
 
-        // check CartoonRepository for latest stored episode
-        Optional<CartoonInfo> cartoonInfoOptional = cartoonRepository.findFirstByCartoonNameOrderByChapterDesc(cartoonName);
-        int latestEpisodeStored = cartoonInfoOptional.map(CartoonInfo::getChapter).orElse(0);
-
-        // get latest episode number from web
         int latestEpisodeOnWeb = webService.getLatestEpisodeNumber(url);
 
-        // loop over getting specific episodes from web and storing them
-        for(int episodeNumber = latestEpisodeStored + 1; episodeNumber <= latestEpisodeOnWeb; episodeNumber++){
+        for (int episodeNumber = latestEpisodeStored + 1; episodeNumber <= latestEpisodeOnWeb; episodeNumber++) {
             System.out.println("Episode number " + episodeNumber);
 
-            // create cartoon in database
-            CartoonInfo newCartoonInfo = new CartoonInfo();
-            newCartoonInfo.setChapter(episodeNumber);
-            newCartoonInfo.setCartoonName(cartoonName);
-            newCartoonInfo.setEndpoint(url + "/" + episodeNumber);
-
+            CartoonInfo newCartoonInfo = toEntity(episodeNumber, cartoonName, url + "/" + episodeNumber);
             newCartoonInfo = cartoonRepository.save(newCartoonInfo);
-
             Long cartoonId = newCartoonInfo.getId();
 
-
-            // add pages
-            List<PageInfoDTO> pages = webService.getEpisode(url, episodeNumber, cartoonId);
-            List<PageInfo> pageInfoList = new ArrayList<>();
-            for(PageInfoDTO page : pages){
-                pageInfoList.add(pageService.toEntity(page));
-            }
-            pageRepository.saveAll(pageInfoList);
+            addPages(url, episodeNumber, cartoonId);
         }
+    }
+
+    private String getUrlFromCartoon(String cartoonName) {
+        List<CartoonConfig> cartoonList = cartoonProperties.getCartoonList();
+
+        for (CartoonConfig cartoon : cartoonList) {
+            if (cartoon.getName().equals(cartoonName)) {
+                return cartoon.getUrl();
+            }
+        }
+        throw new IllegalArgumentException("Cartoon not found");
+    }
+
+    private int getLatestEpisodeNumberStored(String cartoonName) {
+        Optional<CartoonInfo> cartoonInfoOptional = cartoonRepository.findFirstByCartoonNameOrderByChapterDesc(cartoonName);
+        return cartoonInfoOptional.map(CartoonInfo::getChapter).orElse(0);
+    }
+
+    private CartoonInfo toEntity(int chapter, String cartoonName, String endpoint) {
+        CartoonInfo newCartoonInfo = new CartoonInfo();
+        newCartoonInfo.setChapter(chapter);
+        newCartoonInfo.setCartoonName(cartoonName);
+        newCartoonInfo.setEndpoint(endpoint);
+        return newCartoonInfo;
+    }
+
+    private void addPages(String url, int episodeNumber, long cartoonId) throws Exception {
+        List<PageInfoDTO> pages = webService.getEpisode(url, episodeNumber, cartoonId);
+        List<PageInfo> pageInfoList = new ArrayList<>();
+        for (PageInfoDTO page : pages) {
+            pageInfoList.add(pageService.toEntity(page));
+        }
+        pageRepository.saveAll(pageInfoList);
     }
 }
